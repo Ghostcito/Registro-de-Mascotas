82b19a594de43548e7e63337a3b9e20bc2e8c709
Ghostcito <jmguticasani1254@gmail.com>
2024-09-10 23:51:05 -0500

    ActualizarFinalizado

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,16 +10,16 @@ public class HomeController : Controller
     private readonly ILogger<HomeController> _logger;
     private readonly ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger,ApplicationDbContext context)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
-        _context= context;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        var listMascotas=from o in _context.DataMascota select o;
-        return View(listMascotas.ToList());
+        var mascotas = _context.DataMascota.OrderBy(m => m.Id).ToList();
+        return View(mascotas);
     }
 
     public IActionResult Privacy()
@@ -27,13 +27,39 @@ public class HomeController : Controller
         return View();
     }
 
+
+    [HttpPost]
     public IActionResult Delete(long id)
     {
         var mascota = _context.DataMascota.Find(id);
-        if (mascota != null){
-        _context.DataMascota.Remove(mascota);
-        _context.SaveChanges();
+        if (mascota != null)
+        {
+            _context.DataMascota.Remove(mascota);
+            _context.SaveChanges();
         }
+        return RedirectToAction(nameof(Index));
+    }
+    public IActionResult Editar(long id)
+    {
+        var mascota = _context.DataMascota.Find(id);
+        if (mascota == null)
+        {
+            return NotFound();
+        }
+        ViewData["Controlador"] = "Home";
+        ViewData["Action"] = "Actualizar";
+        return View("~/Views/Registro/Index.cshtml", mascota);
+    }
+
+    [HttpPost]
+    public IActionResult Actualizar(Mascota m)
+    {
+        if (m.FechaNacimiento.Kind == DateTimeKind.Unspecified)
+        {
+            m.FechaNacimiento = DateTime.SpecifyKind(m.FechaNacimiento, DateTimeKind.Utc);
+        }
+        _context.DataMascota.Update(m);
+        _context.SaveChanges();
         return RedirectToAction(nameof(Index));
     }
 
